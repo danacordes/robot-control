e00ed70daf8a82e6cfa9aec28b07ac117ec6ce5f
Dana Cordes <dcordes@gmail.com>
2013-07-23 05:32:02 +0000

    integrating network server into robot.py, eliminating the need for server.py added dynamic microstepping. 	<=100 = full step 	<=50 = 1/2 step 	<=25 = 1/4 step 	<=12 = 1/8 step

--- server/robot.py
+++ server/robot.py
@@ -6,6 +6,9 @@ import sys
 import select
 import tty
 import termios
+import asyncore
+import asynchat
+import socket
 
 step_angle = 1.8
 micro_steps = 1
@@ -13,20 +16,61 @@ minimum_step_speed = 0.01
 maximum_step_speed = 0.1 
 reset_pin = 12
 
+class Server(asyncore.dispatcher):
+        def __init__(self, host, port):
+                asyncore.dispatcher.__init__(self)
+                self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.set_reuse_addr()
+                self.bind((host, port))
+                print "listening on ",host,":",port
+                self.listen(5)
+
+        def handle_accept(self):
+                pair = self.accept()
+                if pair is not None:
+                        sock, addr = pair
+                        print 'Incoming connection from %s' % repr(addr)
+                        handler = RoboHandler(sock, addr)
+class RoboHandler(asynchat.async_chat):
+        def __init__(self, sock, addr):
+                asynchat.async_chat.__init__(self, sock)
+
+                self.addr = addr
+                self.ibuffer = []
+                self.obuffer = ""
+                self.set_terminator("\n")
+
+        def collect_incoming_data(self, data):
+                """buffer the data"""
+                self.ibuffer.append(data)
+
+        def found_terminator(self):
+                print self.ibuffer[0]
+		execute_command(self.ibuffer[0])
+                self.ibuffer = []
+
+        def handle_close(self):
+                print 'Disconnecting connection from %s' % repr(self.addr)
+                self.close()
+
+
 class Motor:
 	steps_left = 0
 	speed = 0
 	next_step_tick = 0
 	powered = False
 	direction = 0
+	stepping_multiplier = 1
 	STEPS = 0
 	SPEED = 1
 	mode = STEPS
 	"""Defines a motor"""
-	def __init__(self, name, step_pin, dir_pin):
+	def __init__(self, name, step_pin, dir_pin, ms1_pin, ms2_pin):
 		self.name = name
 		self.step_pin = step_pin
 		self.dir_pin = dir_pin
+		self.ms1_pin = ms1_pin
+		self.ms2_pin = ms2_pin
 
 	def rotate_degrees(self, degrees, speed):
 		self.mode = self.STEPS
@@ -39,10 +83,12 @@ class Motor:
 
 		self.next_step_tick = 0
 
-	def move_speed(self, direction, speed):
+	#def move_speed(self, direction, speed):
+	def move_speed(self, speed):
 		self.mode = self.SPEED
 		self.powered = True
-		self.set_direction(direction)
+		self.set_direction(speed)
+		self.set_stepping(speed)
 		self.set_speed(speed)
 		self.next_step_tick = 0
 
@@ -54,12 +100,37 @@ class Motor:
 			dir_control = GPIO.HIGH if (direction==1) else GPIO.LOW
 			GPIO.output(self.dir_pin, dir_control)
  
+	def set_stepping(self, speed):
+		speed = abs(speed)
+		print "moving", speed
+		if speed > 50:
+			#full step
+			self.stepping_multiplier = 1
+			GPIO.output(self.ms1_pin, GPIO.HIGH)
+			GPIO.output(self.ms2_pin, GPIO.HIGH)
+		elif speed > 25:
+			#1/2 step
+			self.stepping_multiplier = 2
+			GPIO.output(self.ms1_pin, GPIO.LOW)
+			GPIO.output(self.ms2_pin, GPIO.HIGH)
+		elif speed > 12:
+			#1/4 step
+			self.stepping_multiplier = 4
+			GPIO.output(self.ms1_pin, GPIO.HIGH)
+			GPIO.output(self.ms2_pin, GPIO.LOW)
+		else:
+			#1/8 step
+			self.stepping_multiplier = 8
+			GPIO.output(self.ms1_pin, GPIO.LOW)
+			GPIO.output(self.ms2_pin, GPIO.LOW)
+		print "Stepping: ",self.stepping_multiplier
 	def set_speed(self, speed):
 		"""-100 - 100"""
 		self.set_direction(speed)
 		speed = abs(speed)
 		speed = 100 if (speed > 100) else 0 if (speed < 0) else speed
 		self.speed = maximum_step_speed	- ( (speed / 100.0 ) * (maximum_step_speed - minimum_step_speed))
+		self.speed = self.speed/self.stepping_multiplier
 		self.next_step_tick = 0
 		#print "set speed to ", self.speed
 
@@ -91,8 +162,8 @@ class Motor:
 LEFT_MOTOR = 0
 RIGHT_MOTOR = 1
 motors = [
-	Motor('Left Wheel', 7, 11), #name, step pin, dir_pin
-	Motor('Right Wheel', 13, 15)
+	Motor('Left Wheel', 7, 11, 22, 24), #name, step pin, dir_pin, MS1 pin, MS2 pin
+	Motor('Right Wheel', 13, 15, 16, 18)
 ]
 #motors[LEFT_MOTOR] = Motor('Left Wheel', 7, 11)#name, step pin, dir_pin
 #motors[RIGHT_MOTOR] =  Motor('Right Wheel', 13, 15)
@@ -109,6 +180,8 @@ for motor in motors:
 	print "initializing ", motor.name
 	GPIO.setup(motor.step_pin,GPIO.OUT)
 	GPIO.setup(motor.dir_pin,GPIO.OUT)
+	GPIO.setup(motor.ms1_pin,GPIO.OUT)
+	GPIO.setup(motor.ms2_pin,GPIO.OUT)
 
 delay = 0.00001
 #for i in range(0,2*int((360/step_angle)*8)):
@@ -126,15 +199,15 @@ def isData():
         return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
 def control(command, args):
-	print "command:",command," args:",arguments
+	print "command:",command," args:",args
 	obuffer = 'I'
 	if command == 'D':
 		try:
 			wheel = args[0]
-			inP = args[1:].split(',');
+			inP = args[1:].split(',')
 			degrees = int(inP[0])
 			speed = int(inP[1])
-			print "spinning ", wheel, " at speed ", speed, " for ", degrees, " degrees"
+			#print "spinning ", wheel, " at speed ", speed, " for ", degrees, " degrees"
 
 			if wheel == 'L':
 				motors[LEFT_MOTOR].rotate_degrees(degrees, speed)
@@ -149,6 +222,22 @@ def control(command, args):
 			
 	elif command == 'G':
 		obuffer += 'G'
+                try:
+                        wheel = args[0]
+                        speed = int(args[1:])
+                        #print "spinning ", wheel, " at speed ", speed
+
+                        if wheel == 'L':
+                                motors[LEFT_MOTOR].move_speed(speed)
+                        elif wheel == 'R':
+                                motors[RIGHT_MOTOR].move_speed(-speed)
+                        elif wheel == 'B':
+                                motors[LEFT_MOTOR].move_speed(speed)
+                                motors[RIGHT_MOTOR].move_speed(-speed)
+                        obuffer += str(wheel) + str(speed)
+                except Exception as e:
+                        obuffer = "E! Unknown G Command: ",args, e
+
 	elif command == '+':
 		obuffer += '+'
 	elif command == '-':
@@ -164,14 +253,25 @@ def control(command, args):
 
 	return obuffer
 
+def execute_command(command):
+	if command.startswith("C"): #all commands start with C
+		c = command[1]
+		arguments = command[2:]
+		obuffer = control(c, arguments)
+		print command
+	else:
+		print "E! Unknown Control: ",ibuffer
+
 
 ibuffer = '';
 old_settings = termios.tcgetattr(sys.stdin)
+server = Server('', 5555)
 try:
 	tty.setcbreak(sys.stdin.fileno())
 	run_game = True
 
 	while run_game:
+		
 		#output
 		tick = time.clock()
 		motors_moving = False 
@@ -187,6 +287,7 @@ try:
 				#time.sleep(0.001)
 		if (not motors_moving):
 			GPIO.output(reset_pin, GPIO.LOW)
+		
 		#input
 		execute = False
 		if isData():
@@ -197,13 +298,14 @@ try:
 				execute = True
 			ibuffer += c
 		if (execute) :
-			if ibuffer.startswith("C"): #all commands start with C
-				command = ibuffer[1]
-				arguments = ibuffer[2:]
-				obuffer = control(command, arguments)
-				print obuffer
-			else:
-				print "E! Unknown Control: ",ibuffer
+			execute_command(ibuffer)
+#			if ibuffer.startswith("C"): #all commands start with C
+#				command = ibuffer[1]
+#				arguments = ibuffer[2:]
+#				obuffer = control(command, arguments)
+#				print obuffer
+#			else:
+#				print "E! Unknown Control: ",ibuffer
 			#inP = ibuffer.split();
 			#degrees = int(inP[0])
 			#speed = int(inP[1])
@@ -225,8 +327,11 @@ try:
 			except ValueError:
 				print "Not a number"
 
-
+		asyncore.poll()
 finally:
 	GPIO.output(reset_pin, GPIO.LOW)
 	GPIO.cleanup()
 	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+
+
+
--- server/server.py
+++ server/server.py
@@ -3,13 +3,8 @@
 import asyncore
 import asynchat
 import socket
-
-class EchoHandler(asyncore.dispatcher_with_send):
-	def handle_read(self):
-		data = self.recv(8192)
-		if data:
-			self.send(data)
-		print data
+import subprocess
+import StringIO
 
 class RoboHandler(asynchat.async_chat):
 	def __init__(self, sock, addr):
@@ -20,17 +15,35 @@ class RoboHandler(asynchat.async_chat):
 		self.obuffer = ""
 		self.set_terminator("\r\n")
 
+		self.proc = subprocess.Popen(['python', 'robot.py'], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+		self.obuf = StringIO()
+		self.CHUNKSIZE = 32 
+
 	def collect_incoming_data(self, data):
 		"""buffer the data"""
 		self.ibuffer.append(data)
+		#write output
+		self.send(self.proc.stdout.read(self.CHUNKSIZE))
 
 	def found_terminator(self):
 		print self.ibuffer[0]
+
+		self.proc.communicate(self.ibuffer[0])
+		#read from robot output
+		#self.buf.write(self.proc.stdout.read(self.CHUNKSIZE))
+
 		self.ibuffer = []
 
 	def handle_close(self):
 		print 'Disconnecting connection from %s' % repr(self.addr)
 		self.close()
+
+class EchoHandler(asyncore.dispatcher_with_send):
+	def handle_read(self):
+		data = self.recv(8192)
+		if data:
+			self.send(data)
+		print data
 
 
 class Server(asyncore.dispatcher):
@@ -39,6 +52,7 @@ class Server(asyncore.dispatcher):
 		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.set_reuse_addr()
 		self.bind((host, port))
+		print "listening on ",host,":",port 
 		self.listen(5)
 
 	def handle_accept(self):
@@ -49,5 +63,6 @@ class Server(asyncore.dispatcher):
 			handler = RoboHandler(sock, addr)
 
 #server = Server('localhost', 8080)
-server = Server('r2d2', 8080)
+print "starting server" 
+server = Server('', 5555)
 asyncore.loop()
